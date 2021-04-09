c34d1a6d9725a3f9285591919c0cf4e49655d012
codebr3ak <barakaralph@gmail.com>
2021-04-09 15:47:09 +0300

    first commit

---
+++ day 2/variables.py
@@ -0,0 +1,12 @@
+# Day 2, 30 days of python 
+
+first_name = 'Ralph'
+second_name = 'Bunche'
+full_name = 'Ralph Bunche'
+country = 'France'
+city = 'Paris'
+age = 22
+year = 1998
+is_married = 'no'
+is_true = 'yes'
+is_light_on = 'yes'
